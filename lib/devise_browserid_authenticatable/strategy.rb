8207000b19ec367ce9d5ed9645fab4be2cbf819c
Dennis Schubert <software@dsx.cc>
2012-08-19 01:40:42 +0200

    Move files to an own directory

---
+++ lib/devise_browserid_authenticatable/strategy.rb
@@ -0,0 +1,44 @@
+require 'devise/strategies/base'
+require 'net/https'
+
+class Devise::Strategies::BrowseridAuthenticatable < Devise::Strategies::Authenticatable
+  def valid?
+    unless params[:assertion]
+      return false
+    end
+
+    http = Net::HTTP.new(self.browserid_url, 443)
+    http.use_ssl = true
+
+    verification_request = Net::HTTP::Post.new("/verify")
+    verification_request.set_form_data(assertion: params[:assertion], audience: request.host_with_port)
+
+    response = http.request(verification_request)
+    @asserted = JSON.parse(response.body)
+
+    (@asserted["status"] == "okay") and (@asserted["audience"] == request.host_with_port)
+  end
+
+  def authenticate!
+    u = mapping.to.find_by_email(@asserted["email"])
+
+    unless u
+      password = Devise.friendly_token
+      u = mapping.to.create(:email => @asserted["email"], :password => password, :password_confirmation => password)
+      u.skip_confirmation!
+      u.save!
+    end
+
+    success!(u)
+  end
+
+  def browserid_url
+    if Rails.env.production?
+      "browserid.org"
+    else
+      "dev.diresworb.org"
+    end
+  end
+end
+
+Warden::Strategies.add :browserid_authenticatable, Devise::Strategies::BrowseridAuthenticatable
